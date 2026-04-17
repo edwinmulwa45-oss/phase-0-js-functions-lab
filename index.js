3e7f31e3675d3b41add59ddc949f38559fe5af35
Edwin Mulwa <edwinmulwa45@gmail.com>
2026-04-17 12:40:13 +0300

    feat:added index.js

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+// Function 1: calculateTax
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+// Function 2: convertToUpperCase
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
 
+// Function 3: findMaximum
+function findMaximum(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
 
+// Function 4: isPalindrome
+function isPalindrome(word) {
+  let reversed = word.split('').reverse().join('');
+  return word === reversed;
+}
 
+// Function 5: calculateDiscountedPrice
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  let discount = originalPrice * (discountPercentage / 100);
+  return originalPrice - discount;
+}
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+// export all functions
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice
+};
